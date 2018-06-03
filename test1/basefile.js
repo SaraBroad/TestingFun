b4d68b6325adcf1aa5c619a186a9c3c2822d123e
Sara Broad <saraebroad@gmail.com>
2018-06-03 14:54:23 -0400

    js

--- test1/basefile.js
+++ test1/basefile.js
@@ -1,3 +1,7 @@
-var states = ["PENNSYLVANIA", "FLORIDA", "WASHINGTON", "DELAWARE"];
+var lowerCase = function(name) {
 
+var states = ["PENNSYLVANIA", "FLORIDA", "WASHINGTON", "DELAWARE"];
+var names = name.split(" ");
+
+}
 module.exports = basefile;
